f409307c9cfbc63002295ac2bda8e3131389fe2a
udsmdev <114908345+udsmdev@users.noreply.github.com>
2023-01-24 08:30:44 +0400

    Update index.js

--- index.js
+++ index.js
@@ -7,26 +7,11 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "Teachers" && password === "teachers@waa") {
-        alert("You have successfully logged in. To continue please copy & Paste http://10.70.160.207:10001/Work%20Files/index.html");
+    if (username === "Marina" && password === "Marina2@23") {
+        alert("You have successfully logged in. To continue please copy & Paste ");
         location.reload();
-    } if (username === "Mme.Sophie" && password === "mme.sophie@waa") {
-        alert("You have successfully logged in. To continue please copy & Paste http://10.70.160.207:10001/Work%20Files/index.html");
-        location.reload();
-    } if (username === "Mr.Oliver" && password === "mr.oliver@waa") {
-        alert("You have successfully logged in. To continue please copy & Paste http://10.70.160.207:10001/Work%20Files/index.html");
-        location.reload();
-    } if (username === "Ms.Paula" && password === "ms.paula@waa") {
-        alert("You have successfully logged in. To continue please copy & Paste http://10.70.160.207:10001/Work%20Files/index.html");
-        location.reload();
-    } if (username === "Ms.Keely" && password === "ms.keely@waa") {
-        alert("You have successfully logged in. To continue please copy & Paste http://10.70.160.207:10001/Work%20Files/index.html");
-        location.reload();
-    } if (username === "Admin" && password === "Ch@tunette312") {
-        alert("Welcome Admin. You have successfully logged in. To continue please copy & Paste http://10.70.160.207:10001/Work%20Files/index.html");
-        location.reload();
-    } if (username === "Student" && password === "033412") {
-        alert("You have successfully logged in. To continue please copy & Paste http://10.70.160.207:10001/Students/index.html");
+    } if (username === "Fjord" && password === "Fjord2@23") {
+        alert("You have successfully logged in. To continue please copy & Paste ");
         location.reload();
     } else {
         loginErrorMsg.style.opacity = 1;
